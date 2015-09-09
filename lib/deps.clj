7847d5e817b1f07a64187b7c6be3a11f93d80c5d
Paul Bennett <paul@prajnainc.com>
2015-09-09 13:46:37 -0400

    Updated deps

--- lib/deps.clj
+++ lib/deps.clj
@@ -1,1 +1,1 @@
-{:dependencies {org.clojure/clojure {:version "1.6.0"}}, :dev-dependencies {}, :ext-dependencies {}}
+{:dependencies {org.clojure/clojure {:version "1.7.0"}}, :dev-dependencies {}, :ext-dependencies {}}
