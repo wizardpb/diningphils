e3bfb63dfd5c2c28e5c97c1a791f6169e947edf1
Paul Bennett <paul@prajnainc.com>
2017-06-25 18:05:45 -0400

    More convenient user.clj - include comments to include all versions

--- dev/user.clj
+++ dev/user.clj
@@ -5,10 +5,22 @@
              [diningphils.system :as sys]
              [clojure.pprint :as p]
              )
-   (:use
-     diningphils.c-m-async.core
-     diningphils.c-m-async.system
-     diningphils.utils))
+
+   ;; Uncomment one of these :use statements to include the version you want to run.
+
+   ;(:use
+   ;  diningphils.c-m-async.core
+   ;  diningphils.c-m-async.system)
+   ;(:use
+   ;  diningphils.c-m-agents.core
+   ;  diningphils.c-m-agents.system)
+   ;(:use
+   ;  diningphils.res-hi.core
+   ;  diningphils.res-hi.system)
+   ;(:use
+   ;  diningphils.waiter.core
+   ;  diningphils.waiter.system)
+   )
 
 ;(set-debug (range 5))
 (alter-var-root #'*print-level* (constantly 5))
